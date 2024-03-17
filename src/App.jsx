b70aa5eff5656230498658e1f86aa694dd4dd0cb
aniksikder13 <aniksikder9501@gmail.com>
2024-03-17 15:50:58 +0600

    Searching component added

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,19 @@ import Auth from './pages/auth'
 import TrendingMusic from './pages/trendingMusic'
 import ProtectedRoute from './component/auth/Protection'
 import Search from './component/nav/Search'
+import { useCookies } from 'react-cookie'
 
 function App() {
   const [search, setSearch]= useState('')
+  const [cookies, setCookie] = useCookies(['user'])
+  const user = cookies?.user;
+  const isAuthenticated = !!user;
 
   return (
     <Fragment>
       <Header />
       <main className='w-[1600px] m-auto'>
-        <Search onSearch={key => setSearch(key)} />
+        {isAuthenticated && <Search onSearch={key => setSearch(key)} />}
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<ProtectedRoute><TrendingMusic search={search} /></ProtectedRoute>} />
